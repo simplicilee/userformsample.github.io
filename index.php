33b0464ecf5bbadcb117137f3fa70442bb7d0510
simplicilee <solomon.maico.lee>
2017-05-30 16:20:07 +0800

    new updates form

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
-if(isset($_POST['submit'])) {
+if(isset($_POST['submit']))
+ {
   $connect = mysqli_connect ("localhost", "root", "", "userform");
   $username = $_POST['username'];
   $password = $_POST['password'];
@@ -7,15 +8,15 @@ if(isset($_POST['submit'])) {
   mysqli_query($connect, $sql);
   echo '<h2>Hello! '.$username.'</h2> ';
 }
+else
+ echo '<h2>Create Account</h2>'
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<!-- <title>User Password Basic Exam</title>	 -->
- <link  rel="icon"  type="image/x-icon" href="images/icontab.png">
+  <!-- <title>User Password Basic Exam</title>   -->
  <meta charset="utf-8">
  <meta name="userform" content="basic-exam">
- <meta charset="utf-8">
  <meta http-equiv="X-UA-Compatible" content="IE=edge">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
@@ -23,18 +24,16 @@ if(isset($_POST['submit'])) {
 </head>
 <body>
 
-	<div class = "container">
-      <form class="form-signin action="index.php" method="post" ">
-        <!-- <h2 class="form-signin-heading">Please sign in</h2> -->
-        
-        <input type="text" id="username" class="form-control" name="username" placeholder="Username">
-        <input type="password" id="password" class="form-control" name="password" placeholder="Password" minlength="5">
+  <div class = "container">
+      <form class="form-signin action="index.php" method="post" ">     
+        <input type="text" id="username" class="form-control" name="username" placeholder="Username" required>
+        <input type="password" id="password" class="form-control" name="password" placeholder="Password" minlength="5" required>
         <div class="checkbox">
         <label><input type="checkbox" value="remember-me"> Remember me</label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" value="submit" name="submit">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" value="submit" name="submit">Submit</button>
       </form>
-	</div>
+  </div>
 
 
 <!-- Script Section -->
